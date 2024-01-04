8277e51b034157d61426703ad9674b8372534008
ParkSungju01 <psj001002@koreatech.ac.kr>
2024-01-04 18:09:07 +0900

    11주차

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useNavigate } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="box">
+          <div className="href">
+            <a className="hr_login">로그인</a>
+            <a className="hr_new">회원가입</a>
+          </div>
+          <div className="input">
+            <input placeholder="아이디"></input>
+            <input placeholder="비밀번호"></input>
+          </div>
+          <button className="btn_log">로그인</button>
+        </div>
+      </div>
+    </>
   );
 }
 
